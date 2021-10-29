e97864380455b3d095269f72ee64df96eba3ca60
Shruti <sushilkumarbh20@gmail.com>
2021-10-29 19:08:59 +0530

    Login Page added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,7 +12,82 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarMain('LOGIN'),
+      backgroundColor: Colors.white,
+      body:Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 400,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/Image1.png'),
+                fit: BoxFit.fill,
+                ),
+                
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration:BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(143, 148, 251,.5),
+                          blurRadius: 20.0,
+                          offset: Offset(0,10),
+                        ),
+                      ],
+                      
+                    ),
+                    child: Column(
+                      children:<Widget>[
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            border:Border(bottom:BorderSide(color: Colors.grey),),
+                            color: Colors.white,
+                          ),
+
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border :InputBorder.none,
+                              hintText: "Mobile Number",
+                              hintStyle: TextStyle(color:Colors.grey[400],)
+                            ),
+                          )
+                        )
+                      ]
+                    ),
+                    ),
+
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromRGBO(143, 150, 251,1),
+                        Color.fromRGBO(143, 150, 251,.9),
+                    ])
+                  ),
+                  child: Center(
+                    child: Text("Get OTP",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+                    ),),
+                  )
+                ],
+              ),)
+          ],
+        ),
+      ),
     );
   }
 }
