df462c0e634ab84cae153bb2fef843a496b4cd97
Suraj Patel <sp814729@gmail.com>
2021-10-30 17:00:43 +0530

    Otp page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ride_share_app/screens/login_screen.dart';
+import 'package:ride_share_app/screens/otp.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white
       ),
-      home: const LoginScreen(),
+      home: const Otp(),
     );
   }
 }
