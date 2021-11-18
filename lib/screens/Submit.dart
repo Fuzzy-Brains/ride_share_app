36eee4dd671baa32441eb61274d06649ff3141d1
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2021-11-18 21:13:43 +0530

    booking part done

--- lib/screens/Submit.dart
+++ lib/screens/Submit.dart
@@ -1,5 +1,11 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:random_string/random_string.dart';
+import 'package:ride_share_app/backend/database.dart';
 import 'package:ride_share_app/screens/user_screen.dart';
 import 'package:ride_share_app/utils/constants.dart';
 
@@ -8,8 +14,8 @@ import 'package:ride_share_app/utils/constants.dart';
 
 class SubmitPage extends StatefulWidget {
   final User? user;
-  final String? source;
-  final String? destination;
+  final String source;
+  final String destination;
   const SubmitPage({Key? key, required this.user,required this.source,required this.destination }) : super(key: key);
   @override
   _SubmitPageState createState() => _SubmitPageState();
@@ -17,6 +23,30 @@ class SubmitPage extends StatefulWidget {
 
 class _SubmitPageState extends State<SubmitPage> {
   bool showPassword = false;
+  String? code;
+  Database db= Database();
+  QuerySnapshot? snapshot;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      code = randomBetween(100000, 999999).toString();
+    });
+  }
+
+  confirm() async{
+    await db.findVehicle(widget.source).then((value) {
+      snapshot = value;
+    });
+    String reg_no = snapshot!.docs[0].get('reg_no');
+    String owner = snapshot!.docs[0].get('owner');
+    await db.confirmRide(widget.user, widget.source, widget.destination, code!, reg_no).then((value) {
+      db.saveVehicleData(widget.user, reg_no, owner, widget.destination, false);
+      Fluttertoast.showToast(msg: "Ride Booked Successfully");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,23 +87,17 @@ class _SubmitPageState extends State<SubmitPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {
-
-              },
-              child: Container(
-
-                width: size.width * 0.6,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: Text(
-                    'Add A Vehicle', style: TextStyle(
-                      fontWeight: FontWeight.bold, color: white, fontSize: 24),
-                  ),
+            Container(
+              width: size.width * 0.6,
+              height: 60,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Text(
+                  '${randomBetween(100000, 999999)}' , style: TextStyle(
+                    fontWeight: FontWeight.bold, color: white, fontSize: 24),
                 ),
               ),
             ),
@@ -101,16 +125,39 @@ class _SubmitPageState extends State<SubmitPage> {
               onTap: () {
 
               },
+              child: Center(
+                child: Container(
+                  width: size.width * 0.9,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: white,
+                    borderRadius: BorderRadius.circular(12),
+
+                  ),
+                  child: Center(
+                    child: Text(
+                      'You can get your bike at ${widget.source}. Collect your bike in 30 mins otherwise'
+                          ' your booking will be cancelled.', style: TextStyle(
+                        fontWeight: FontWeight.bold, color: black, fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                confirm();
+              },
               child: Container(
-                width: size.width * 0.9,
-                height: 80,
+                width: size.width * 0.6,
+                height: 60,
                 decoration: BoxDecoration(
                   color: primaryColor,
                   borderRadius: BorderRadius.circular(12),
                 ),
                 child: Center(
                   child: Text(
-                    'Take in 30 min at ${widget.source}', style: TextStyle(
+                    'Confirm Your Ride' , style: TextStyle(
                       fontWeight: FontWeight.bold, color: white, fontSize: 24),
                   ),
                 ),
